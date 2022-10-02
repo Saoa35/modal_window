67058483e4c0bb8d6aa69ddd5f5ba99580612462
Saoa35 <saoamah@gmail.com>
2022-10-02 17:49:21 +0200

    created Modal component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,18 @@
 import React, { useState } from 'react';
 import '../../index.css'
 
+const Modal = ({ open, setOpen, children }) => (
+  <div className={`overlay animated ${open ? 'show' : ''}`}>
+    <div className="modal">
+      <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+        <title />
+        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+      </svg>
+      {children}
+    </div>
+  </div>
+)
+
 function App() {
 
   const [open, setOpen] = useState(false);
@@ -10,15 +22,9 @@ function App() {
 
     <button className='open-modal-btn' onClick={() => setOpen(true)}>✨ Open window</button> 
 
-      <div className={`overlay animated ${open ? 'show' : ''}`}>
-          <div className="modal">
-            <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
-              <title />
-              <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-            </svg>
-            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"  alt='modal pic'/>
-          </div>
-        </div>
+      <Modal open={open} setOpen={setOpen} >
+        <h2>Modal window is opened</h2>
+      </Modal>
 
     {/* {open && 
       (<div className="overlay">
