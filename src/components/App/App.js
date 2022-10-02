bee4d4a1bad1e53f67c53717ad305f551678e5bf
Saoa35 <saoamah@gmail.com>
2022-10-02 16:06:36 +0200

    created modal window app

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../index.css'
+
+function App() {
+  return (
+    <div className="App">
+
+    <button className='open-modal-btn'>✨ Open window</button>
+
+    </div>
+  );
+}
+
+export default App;
